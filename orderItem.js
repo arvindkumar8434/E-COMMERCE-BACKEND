7bf7eb7d54700739d12d3e0f770516cb3caa77d5
arvindkumar8434 <arvindkrr2003@gmail.com>
2025-09-05 22:17:15 +0530

    Create orderItem.js

---
+++ orderItem.js
@@ -0,0 +1,7 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('OrderItem', {
+    id: { type: DataTypes.INTEGER, primaryKey:true, autoIncrement:true },
+    quantity: { type: DataTypes.INTEGER, allowNull:false, defaultValue:1 },
+    unitPrice: { type: DataTypes.DECIMAL(10,2), allowNull:false }
+  }, { timestamps:false, tableName:'order_items' });
+};
